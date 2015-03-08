9c2757d57d3fe46c85472158271366b12e35caca
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-03-08 20:31:57 +0100

    provided flags and format parameters

--- tasks/flag-sprite.js
+++ tasks/flag-sprite.js
@@ -10,32 +10,49 @@
  * @license MIT https://raw.github.com/tkoomzaaskz/grunt-flag-sprite/master/LICENSE
  */
 
+var path = require('path'),
+    allowedFormats = ['1x1', '4x3'];
+
 module.exports = function (grunt) {
-    grunt.config('svg_sprite', {
-        flags: {
-            src: [
-                'node_modules/flag-svg-collection/flags/4x3/pl.svg',
-                'node_modules/flag-svg-collection/flags/4x3/de.svg',
-                'node_modules/flag-svg-collection/flags/4x3/us.svg',
-                'node_modules/flag-svg-collection/flags/4x3/gb.svg',
-            ],
-            dest: 'flags',
-            options: {
-                mode: {
-                    css: {// Activate the «css» mode
-                        render: {
-                            css: true,
-                            scss: true,
-                            styl: true,
-                            less: true
+    grunt.registerMultiTask('flag-sprite', '', function () {
+        var flagpath = 'node_modules/flag-svg-collection/flags/',
+            options = this.options(),
+            flags = options.flags,
+            format = options.format;
+        if (!flags || flags.length == 0) {
+            grunt.fail.warn('Incorrect flags. Set flags array.');
+        }
+        if (!format || allowedFormats.indexOf(format) < 0) {
+            grunt.fail.warn('Incorrect format. Choose one of: ' + allowedFormats.join(', ') + '.');
+        }
+        // this.requiresConfig ??
+        // validate format: either 1x1 or 4x3
+        // validate flags: exist
+        var sources = flags.map(function(flag) {
+            return path.normalize(flagpath + format + '/' + flag + '.svg')
+        });
+        grunt.log.oklns('Generating sprite for flags: ' + flags.join(', '));
+
+//        console.log(this.files);
+//        console.log(this.filesSrc);
+        grunt.config('svg_sprite', { // grunt.config.merge ??
+            flags: {
+                src: sources,
+                dest: 'flags',
+                options: {
+                    mode: {
+                        css: {// Activate the «css» mode
+                            render: {
+                                css: true,
+                                scss: true,
+                                styl: true,
+                                less: true
+                            }
                         }
                     }
                 }
             }
-        }
-    });
-
-    grunt.registerMultiTask('flag-sprite', '', function () {
+        });
         grunt.task.run('svg_sprite:flags');
     });
 
